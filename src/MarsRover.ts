7ab7c82b81df32ab0504328b805cf950e84a8783
Alberto González Álvarez <alberjga97@gmail.com>
2024-03-15 15:59:36 +0000

    ref: moving forward refactor

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -2,14 +2,10 @@ import { Grid } from "./Grid.js"
 
 export class MarsRover {
   execute(commands: string) {
-    if (commands === "M") {
-      return "0:1:N"
-    }
+    if (!commands) return "0:0:N"
 
-    if (commands === "MM") {
-      return "0:2:N"
-    }
+    const extractedCommands = commands.split("")
 
-    return "0:0:N"
+    return `0:${extractedCommands.length}:N`
   }
 }
