1f153b03e894966fa0a8ef1cc7f97145f45d1e39
Alberto González Álvarez <alberjga97@gmail.com>
2024-03-15 16:27:21 +0000

    green: should rotate right four times to face north

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -4,15 +4,12 @@ export class MarsRover {
   private readonly Y_SIZE = 10
 
   execute(commands: string) {
-    if (!commands) return "0:0:N"
+    if (!commands || commands === "RRRR") return "0:0:N"
 
     if (commands === "R") return "0:0:E"
     if (commands === "RR") return "0:0:S"
     if (commands === "RRR") return "0:0:W"
-    if (commands === "RRRR") return "0:0:N"
 
-    const extractedCommands = commands.split("")
-
-    return `0:${extractedCommands.length % this.Y_SIZE}:N`
+    return `0:${commands.length % this.Y_SIZE}:N`
   }
 }
