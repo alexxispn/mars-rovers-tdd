41171d9d6fa10df4cc1b6b784662585fd0805cf1
Alberto González Álvarez <alberjga97@gmail.com>
2024-03-15 16:37:45 +0000

    ref: move N times to right

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -3,12 +3,14 @@ import { Grid } from "./Grid.js"
 export class MarsRover {
   private readonly Y_SIZE = 10
 
+  private readonly directions = ["N", "E", "S", "W"]
+
   execute(commands: string) {
     if (!commands || commands === "RRRR") return "0:0:N"
 
-    if (commands === "R") return "0:0:E"
-    if (commands === "RR") return "0:0:S"
-    if (commands === "RRR") return "0:0:W"
+    if (commands.split("").every((c) => c === "R")) {
+      return `0:0:${this.directions[commands.length % this.directions.length]}`
+    }
 
     return `0:${commands.length % this.Y_SIZE}:N`
   }
