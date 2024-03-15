90476723078b08fb1aceb7b61abff2708018adec
Alberto González Álvarez <alberjga97@gmail.com>
2024-03-15 15:56:07 +0000

    green: should calculate position when moving

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -1,8 +1,15 @@
 import { Grid } from "./Grid.js"
 
 export class MarsRover {
-  execute(command: string) {
-    if (command === "M") return "0:1:N"
+  execute(commands: string) {
+    if (commands === "M") {
+      return "0:1:N"
+    }
+
+    if (commands === "MM") {
+      return "0:2:N"
+    }
+
     return "0:0:N"
   }
 }
