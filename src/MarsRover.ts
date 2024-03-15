85aa4448779d253831295b27bf2a3286ad0a34ff
Alberto González Álvarez <alberjga97@gmail.com>
2024-03-15 16:06:46 +0000

    ref: grid size

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -1,11 +1,13 @@
 import { Grid } from "./Grid.js"
 
 export class MarsRover {
+  private readonly Y_SIZE = 10
+
   execute(commands: string) {
     if (!commands) return "0:0:N"
 
     const extractedCommands = commands.split("")
 
-    return `0:${extractedCommands.length % 10}:N`
+    return `0:${extractedCommands.length % this.Y_SIZE}:N`
   }
 }
